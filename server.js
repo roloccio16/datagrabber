a2283a736dc389edbd5b0075e48d1f1aa7ecfc47
datadiego <juandiegomariscal@gmail.com>
2024-10-03 11:02:15 +0200

    update: "quitamos log"

--- server.js
+++ server.js
@@ -25,10 +25,12 @@ app.use((req, res, next) => {
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+/*
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
 });
+*/
 
 /*
 ░█▀▀░█▀█░█▀▄░█▀█░█▀█░▀█▀░█▀█░▀█▀░█▀▀
